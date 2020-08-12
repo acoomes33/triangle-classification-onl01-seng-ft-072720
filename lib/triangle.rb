60667cb7d38136ffdb2770bf520d4784c17622aa
Alan Coomes <acoomes1@cord.edu>
2020-08-12 20:51:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,7 @@ class Triangle
       end
     elsif @side1 == @side2 && @side1 == @side3
       :equilateral
-      # elsif @side1 == @side2 || @side == @side3
+      # elsif @side1 == @side2 || @side1 == @side3 || 
       # :isoceles
       # elsif @side1 != @side2 != @side3
       # :scalene
