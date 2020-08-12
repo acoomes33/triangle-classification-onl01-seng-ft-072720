274dfd4c7926a3a7d9e0136679d309acb6f5da03
Alan Coomes <acoomes1@cord.edu>
2020-08-12 21:08:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -40,6 +40,7 @@ class Triangle
         raise TriangleError
       rescue TriangleError => error
           puts error.message
+        end 
     elsif equilateral?
       :equilateral
     elsif isoceles? 
